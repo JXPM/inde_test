b20d19954c5d05f6d1a8b293307a50fba85b1f38
JXPM <bilejohan04@gmail.com>
2026-02-23 14:44:51 +0100

    maj de readme

--- Journal.sh
+++ Journal.sh
@@ -3,8 +3,8 @@ git init
 git branch -M main
 git add .
 git commit -m "first commit"
-gh repo create indé_test --public
-git remote add origin https://github.com/JXPM/indé_test.git
+gh repo create inde_test --public
+git remote add origin https://github.com/JXPM/inde_test.git
 git push --set-upstream origin main
 
 #fichier Maj et push
@@ -12,3 +12,11 @@ git status
 git add .
 git commit -m "maj de readme"
 git push origin main
+
+
+python3 -m venv nike_venv
+
+source nike_venv/bin/activate
+
+pip install -r requierements.txt
+python3 nike_formulaire.py
